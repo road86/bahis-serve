b15f67d41404ae0fd1563772bf6e95e58da41f27
Sharful Islam <34204530+sharful09@users.noreply.github.com>
2023-08-09 13:40:37 +0600

    removed dead code in model

--- kobocat/onadata/apps/bh_module/models.py
+++ kobocat/onadata/apps/bh_module/models.py
@@ -5,7 +5,6 @@ from django.contrib.auth.models import User
 class DeskVersion(models.Model):
     user = models.ForeignKey(User)
     desk_version = models.CharField(max_length=20)
-    # instance_id = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
